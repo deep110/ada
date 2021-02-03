47ec19416462ab4cd79332ad0a89c91aa5247136
Deepankar Agrawal <deepankar11093@gmail.com>
2021-02-04 01:49:18 +0530

    merge examples into one

--- examples/draw_hollow.rs
+++ examples/draw_hollow.rs
@@ -1,10 +1,27 @@
 extern crate ada;
 
-use ada::{shape, Canvas};
 use minifb::{Key, Window, WindowOptions};
+
+use ada::{shape, Canvas};
 
 const WIDTH: usize = 512;
 const HEIGHT: usize = 512;
+
+fn draw_line(canvas: &mut Canvas) {
+    shape::draw_line2d(50, 50, 200, 300, canvas, &ada::color::WHITE);
+}
+
+fn draw_rectangle(canvas: &mut Canvas) {
+    shape::draw_rect2d(50, 100, 100, 150, canvas, &ada::color::RED);
+}
+
+fn draw_circle(canvas: &mut Canvas) {
+    shape::draw_ellipse2d(350, 200, 100, 100, canvas, &ada::color::BLUE);
+}
+
+fn draw_ellipse(canvas: &mut Canvas) {
+    shape::draw_ellipse2d(400, 300, 100, 50, canvas, &ada::color::GREEN);
+}
 
 fn main() {
     let mut buffer: Vec<u32> = vec![0; WIDTH * HEIGHT];
@@ -29,11 +46,11 @@ fn main() {
     let mut cbuffer: Vec<u8> = vec![0; 4 * WIDTH * HEIGHT];
     let mut canvas = Canvas::new(WIDTH, HEIGHT, &mut cbuffer[..]).unwrap();
 
-    let white = ada::color::WHITE;
-    let circle = shape::Ellipse2D::new(250, 250, 150, 150);
-
     while window.is_open() && !window.is_key_down(Key::Escape) {
-        canvas.draw(&circle, &white, false);
+        draw_line(&mut canvas);
+        draw_rectangle(&mut canvas);
+        draw_circle(&mut canvas);
+        draw_ellipse(&mut canvas);
 
         for (i, pix) in buffer.iter_mut().enumerate() {
             let c = canvas.get_color((i % WIDTH) as i32, (i / WIDTH) as i32);
